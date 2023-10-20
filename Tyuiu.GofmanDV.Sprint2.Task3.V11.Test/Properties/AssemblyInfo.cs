b3c3bfda15ea7cf3e87339f8553990b2c898c6cc
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-20 21:26:16 +0500

    Task3

---
+++ Tyuiu.GofmanDV.Sprint2.Task3.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GofmanDV.Sprint2.Task3.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GofmanDV.Sprint2.Task3.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("125cef0f-d22f-41ef-a7c5-fee5c1fb05e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GofmanDV.Sprint2.Task3.V11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GofmanDV.Sprint2.Task3.V11.Lib;
+namespace Tyuiu.GofmanDV.Sprint2.Task3.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Гофман Д. В. | СмартБ 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Гофман Даниил Викторович | СмартБ 23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else,                            *");
+            Console.WriteLine("* где пользователь вводит значение переменной X с клавиатуры.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Введите значение переменной X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var res = ds.Calculate(x);
+            Console.WriteLine(Math.Round(res, 3));
+            Console.ReadKey();
+        }
+    }
+}
