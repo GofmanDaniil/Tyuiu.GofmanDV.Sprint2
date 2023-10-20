9bf1a64a7ba327b7d18f7bfb6bc1a655f0153ae1
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-20 22:09:02 +0500

    Task7

---
+++ Tyuiu.GofmanDV.Sprint2.Task7.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GofmanDV.Sprint2.Task7.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GofmanDV.Sprint2.Task7.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("784cfb4c-7e82-45cd-b53c-a561d04b5ef5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
