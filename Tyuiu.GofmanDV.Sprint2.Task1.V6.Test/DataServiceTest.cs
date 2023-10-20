3db5d5baeb29db1b7afecc9eae34c2f683225b96
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-20 20:36:00 +0500

    Task1

---
+++ Tyuiu.GofmanDV.Sprint2.Task1.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GofmanDV.Sprint2.Task1.V6.Lib;
+
+namespace Tyuiu.GofmanDV.Sprint2.Task1.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetLogicOperationsTest()
+        {
+            DataService ds = new DataService();
+            bool[] wait = new bool[6] { false, false, true, false, true, false };
+            int a = 915, b = 169, c = 174, d = 133;
+            CollectionAssert.AreEqual(wait, ds.GetLogicOperations(a, b, c, d));
+        }
+    }
+}
---
+++ Tyuiu.GofmanDV.Sprint2.Task1.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GofmanDV.Sprint2.Task1.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GofmanDV.Sprint2.Task1.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f6064a3-5702-439c-8a42-402fcdc0af10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
